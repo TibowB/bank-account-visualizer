ca9e0a252c4c65a7caefb7497dacc5265e9e6a73
Thibault Bruggeman <thibault.bruggeman@zaclys.net>
2023-02-08 21:20:51 +0100

    Update App.vue

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,17 +1,23 @@
 <script setup lang="ts">
-import { client } from './api/client';
-const handleImportFile = () => {
-  client.post("/test", {
-    name: "toto"
-  })
-}
+import { ref } from "vue";
+import { importFile } from "./api/import";
+const fileInput = ref<HTMLInputElement>();
+
+const handleImportFile = async () => {
+  if (fileInput.value === undefined || fileInput.value.files === null) {
+    return;
+  }
+
+  const file = fileInput.value.files[0];
+
+  await importFile(file);
+};
 </script>
 
 <template>
   <article data-theme="dark">
     <label for="file">Import File</label>
-    <input type="file" name="file" />
+    <input ref="fileInput" type="file" name="file" />
     <button @click="handleImportFile">Import</button>
   </article>
 </template>
-
