7a26cc6e9d3abe64c2c46649e3c48fd756a84ddb
Thibault Bruggeman <thibault.bruggeman@zaclys.net>
2023-02-11 11:44:33 +0100

    Update account.py

--- back/account.py
+++ back/account.py
@@ -3,8 +3,8 @@ class Account:
     def __init__(self, account) -> None:
         self.account_id = account.account_id
         self.routing_number = account.routing_number
-        self.branch_id = account.branch_id    
-        self.institution = account.institution
+        self.branch_id = account.branch_id  
+        self.statement = account.statement  
 
     def __str__(self) -> str:
         return f"{self.account_id} {self.routing_number} {self.branch_id}"
