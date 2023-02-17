1625a7598946a53d1ef50b239f7a679ba0d13c32
Thibault Bruggeman <thibault.bruggeman@zaclys.net>
2023-02-17 17:30:51 +0100

    Update import_service

--- back/services/import_service.py
+++ back/services/import_service.py
@@ -1,6 +1,7 @@
 from ofxparse import OfxParser
 from fastapi import UploadFile
-
+from dtos import AccountDto
+from typing import List
 from entities import Account, Statement, Transaction
 
 class ImportService:
@@ -8,17 +9,44 @@ class ImportService:
     def __init__(self, ofx_parser: OfxParser) -> None:
         self.ofx_parser = ofx_parser
 
+
     def import_ofx_file(self, file: UploadFile):
         ofx = self.ofx_parser.parse(file)
 
-        account = Account(ofx.account)
+        account = self.save_account_from_ofx(ofx)
 
-        statement = Statement(account.statement)
+        statement = self.save_statement_from_ofx(ofx, account)
 
-        transactions: list[Transaction] = list()
-
-        for transaction in statement.transactions:
-            trn = Transaction(transaction)
-            transactions.append(trn)
+        self.save_transactions_from_ofx(ofx, statement)
             
-        return {"account": account, "statement": statement, "transactions": transactions}
+        return {"account": account.__data__, "statement": statement.__data__}
+
+    def save_account_from_ofx(self, ofx):
+        account = ofx.account
+        Account.create(
+            account_id=account.account_id, 
+            routing_number=account.routing_number, 
+            branch_id=account.branch_id)
+
+    def save_statement_from_ofx(self, ofx, account: Account):
+        statement = ofx.account.statement
+        Statement.create(
+            start_date=statement.start_date,
+            end_date=statement.end_date,
+            balance=statement.balance,
+            available_balance=statement.available_balance,
+            account=account)
+
+    def save_transactions_from_ofx(self, ofx, statement: Statement):
+        transaction_list = [
+            Transaction.create(
+                payee = transaction.payee,
+                type = transaction.type,
+                date = transaction.date,
+                amount = transaction.amount,
+                tid = transaction.id,
+                statement = statement) 
+            for transaction in ofx.account.statement.transactions
+            ]
+        Transaction.bulk_create(transaction_list, batch_size=100)
+        
